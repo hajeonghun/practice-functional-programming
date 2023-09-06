4f3228e99931995b1bb4b2e3127164d24108e3de
하정훈 <hajh1994@gmail.com>
2023-09-06 22:02:16 +0900

    chore: map, filter, reduce 작성해보기

---
+++ src/01/reduce.js
@@ -0,0 +1,28 @@
+import {map} from "./map.js";
+
+const products = [
+    {name: '반팔티', price: 15000},
+    {name: '긴팔티', price: 20000},
+    {name: '핸드폰케이스', price: 15000},
+    {name: '후드티', price: 30000},
+    {name: '바지', price: 25000}
+];
+
+const reduce = (f, acc, iter) => {
+    if(!iter){
+        iter = acc[Symbol.iterator]();
+        acc = iter.next().value;
+    }
+
+    for (const a of iter) {
+        acc = f(acc, a);
+    }
+
+    return acc;
+}
+
+const add = (a, b) => a + b;
+console.log(reduce(add, 0, [1, 2, 3, 4, 5])); // 15
+console.log(reduce(add, [1, 2, 3, 4, 5])); // 15
+
+console.log(reduce(add, map(item => item.price, products))); // 총합
